305e7c0d52ac328568d59db5cdd1ba1fbd2cfb86
Hazel <bguoac@connect.ust.hk>
2023-05-15 13:16:42 -0700

    add_image_features_analysis

---
+++ believability_factors_img.py
@@ -0,0 +1,99 @@
+from matplotlib import pyplot as plt
+
+import argparse
+import subprocess
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+
+def plot_distribution(few_feature, many_feature, feature_name, pval, output_dir='distributions/'):
+    df = pd.DataFrame({"many": many_feature, "few": few_feature})
+    ax = df.plot.hist(bins=100, alpha=0.5)
+    if pval:
+        plt.annotate(f"p-value = {pval}",
+                     xy=(0.5, 0.5), xycoords='axes fraction')
+    plt.gca().set(title=feature_name.replace("_", " "))
+    plt.savefig(output_dir + feature_name, dpi=200)
+    plt.close()
+
+
+def process_numerical_feature(few, many, feature, output_dir='distributions/', filter=False):
+    print(f"processing {feature}...")
+    few_feature = few[feature]
+    many_feature = many[feature]
+
+    if filter:
+        # filter out any features that doesn't meet the level of significance
+        _statistic, pvalue = stats.mannwhitneyu(
+            few_feature.to_numpy(), many_feature.to_numpy())
+        if pvalue > 0.1:
+            print()
+            print()
+            return
+
+    few_mean = few_feature.mean()
+    few_median = few_feature.median()
+    few_std = few_feature.std()
+    print("believable by few:")
+    print("mean, median, std")
+    print(few_mean, few_median, few_std)
+    print()
+
+    many_mean = many_feature.mean()
+    many_median = many_feature.median()
+    many_std = many_feature.std()
+    print("believable by many:")
+    print("mean, median, std")
+    print(many_mean, many_median, many_std)
+
+    # _statistic, pvalue = stats.ttest_ind(few_feature.to_numpy(), many_feature.to_numpy(), equal_var=False)
+    # _statistic, pvalue = stats.kruskal(few_feature.to_numpy(), many_feature.to_numpy())
+    _statistic, pvalue = stats.mannwhitneyu(
+        few_feature.to_numpy(), many_feature.to_numpy())
+    print(f"pvalue: {pvalue}")
+    print()
+    print()
+
+    plot_distribution(few_feature, many_feature, feature, pvalue, output_dir)
+
+
+def process_cat_feature(few, many, feature):
+    print("processing " + str(feature) + "...")
+    few = few[feature].value_counts()
+    many = many[feature].value_counts()
+    print("believable by few:")
+    print(few)
+    print()
+    print("believable by many:")
+    print(many)
+
+    sent_table = np.stack([many.to_numpy(), few.to_numpy()])
+    _statistic, pvalue, _dof, _expected_freq = stats.chi2_contingency(
+        sent_table)
+    print(f"pvalue: {pvalue}")
+    print()
+    print()
+
+
+def process_img_features(few, many):
+    numerical_features_list = ['media_count', 'visual_sentiment_first_negative', 'visual_sentiment_first_neutral', 'visual_sentiment_first_positive',
+                               'visual_sentiment_avg_negative', 'visual_sentiment_avg_neutral', 'visual_sentiment_avg_positive']
+    cat_features_list = ['celebrity_presence', 'text_presence']
+
+    for feature in numerical_features_list:
+        process_numerical_feature(
+            few, many, feature, "distributions/IMG/", False)
+    for feature in cat_features_list:
+        process_cat_feature(few, many, feature)
+
+
+if __name__ == "__main__":
+
+    print("processing image features")
+    img_few = pd.read_csv(
+        './datasets/believable_by_few_img_features.csv', sep=',')
+    img_many = pd.read_csv(
+        './datasets/believable_by_many_img_features.csv', sep=',')
+    img_features = img_many.columns.tolist()[2:]
+    process_img_features(img_few, img_many)
--- models/data_utils.py
+++ models/data_utils.py
@@ -1,56 +1,64 @@
 import pandas as pd
 import numpy as np
 
+
 def get_test_set():
     believable_by_few = pd.read_csv('../believable_by_few.csv', sep=',')
     believable_by_many = pd.read_csv('../believable_by_many.csv', sep=',')
-    
+
     liwc_few = pd.read_csv('../believable_by_few_liwc.csv', sep=',')
     liwc_many = pd.read_csv('../believable_by_many_liwc.csv', sep=',')
     # we are dropping emotion as we are obtaining emotion values from elsewhere
     liwc_few = liwc_few.drop(columns='emotion')
     liwc_many = liwc_many.drop(columns='emotion')
-    
-    believable_by_few = believable_by_few.merge(liwc_few, on="tweetId", how="inner")
-    believable_by_many = believable_by_many.merge(liwc_many, on="tweetId", how="inner")
-    
+
+    believable_by_few = believable_by_few.merge(
+        liwc_few, on="tweetId", how="inner")
+    believable_by_many = believable_by_many.merge(
+        liwc_many, on="tweetId", how="inner")
+
     return believable_by_few.iloc[-20:], believable_by_many.iloc[-20:]
+
 
 def get_train_set():
     believable_by_few = pd.read_csv('../believable_by_few.csv', sep=',')
     believable_by_many = pd.read_csv('../believable_by_many.csv', sep=',')
-    
+
     liwc_few = pd.read_csv('../believable_by_few_liwc.csv', sep=',')
     liwc_many = pd.read_csv('../believable_by_many_liwc.csv', sep=',')
     # we are dropping emotion as we are obtaining emotion values from elsewhere
     liwc_few = liwc_few.drop(columns='emotion')
     liwc_many = liwc_many.drop(columns='emotion')
-    
-    believable_by_few = believable_by_few.merge(liwc_few, on="tweetId", how="inner")
-    believable_by_many = believable_by_many.merge(liwc_many, on="tweetId", how="inner")
-        
+
+    believable_by_few = believable_by_few.merge(
+        liwc_few, on="tweetId", how="inner")
+    believable_by_many = believable_by_many.merge(
+        liwc_many, on="tweetId", how="inner")
+
     return believable_by_few.iloc[:50], believable_by_many.iloc[:50]
+
 
 def extract_features(data):
     features = []
     for idx in range(data.shape[0]):
         row = data.iloc[idx]
-        
+
         # we first process the metadata
-        meta_data = np.concatenate((row[2:7].to_numpy(dtype=np.float64), row[8:10].to_numpy(dtype=np.float64)))
+        meta_data = np.concatenate(
+            (row[2:7].to_numpy(dtype=np.float64), row[8:10].to_numpy(dtype=np.float64)))
         meta_data = np.log(meta_data, where=(meta_data != 0))
         meta_data = np.append(meta_data, row['user_verified'])
-        
+
         # now sentiment
         sentiment = np.zeros(1)
         if row["sentiment"] == 'Negative':
             sentiment[0] = -1
         elif row["sentiment"] == 'Positive':
             sentiment[0] = 1
-                
+
         # emotions
         emotions = row[13:20].to_numpy(dtype=np.float64)
-                
+
         # topic
         topic = np.zeros(1)
         if row["topic"] == 'politics':
@@ -63,27 +71,29 @@ def extract_features(data):
             topic[0] = 4
         elif row['topic'] == 'religion':
             topic[0] = 5
-                
+
         # toxicity
         toxicity = row[21:28].to_numpy(dtype=np.float64)
-        
+
         # liwc
-        liwc_columns = ['WC', 'WPS', 'they', 'number', 'adverb', 'Drives', 'power', 'cause', 'discrep', 'friend', 'politic', 'leisure', 'relig', 'need', 'fatigue', 'allure', 'visual', 'focuspast', 'Conversation', 'AllPunc', 'Comma', 'Exclam', 'OtherP']
+        liwc_columns = ['WC', 'WPS', 'they', 'number', 'adverb', 'Drives', 'power', 'cause', 'discrep', 'friend', 'politic', 'leisure',
+                        'relig', 'need', 'fatigue', 'allure', 'visual', 'focuspast', 'Conversation', 'AllPunc', 'Comma', 'Exclam', 'OtherP']
         liwc = row[liwc_columns].to_numpy(dtype=np.float64)
-        
-        row_features = np.concatenate((meta_data, sentiment, emotions, topic, toxicity, liwc))
+
+        row_features = np.concatenate(
+            (meta_data, sentiment, emotions, topic, toxicity, liwc))
         features.append(row_features)
     features = np.array(features)
-    
+
     return features
 
 
 def eval_metrics(labels, targets):
     if len(labels) != len(targets):
         raise Exception("labels and targets must have the same length")
-    
+
     true_pos = true_neg = false_pos = false_neg = 0
-    
+
     for i, a in enumerate(labels):
         if a == 1 and targets[i] == 1:
             true_pos += 1
@@ -93,15 +103,15 @@ def eval_metrics(labels, targets):
             false_pos += 1
         else:   # a == 0 and targets[i] == 1
             false_neg += 1
-    
+
     accuracy = (true_pos + true_neg) / len(labels)
     precision = true_pos / (true_pos + false_pos)
     recall = true_pos / (true_pos + false_neg)
     f1 = 2 * precision * recall / (precision + recall)
-    
+
     print(f'accuracy: {accuracy}')
     print(f'precision: {precision}')
     print(f'recall: {recall}')
     print(f'f1 score: {f1}')
-    
+
     return accuracy, precision, recall, f1
