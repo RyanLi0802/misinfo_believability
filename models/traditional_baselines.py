f0125cfad9bb68f3cd9385f39201367a52acf27f
Gloria <gloria@Glorias-MacBook-Pro.local>
2023-08-24 00:33:53 +0800

    update model

--- models/traditional_baselines.py
+++ models/traditional_baselines.py
@@ -39,13 +39,13 @@ test_targets = [0] * few_test.shape[0] + [1] * many_test.shape[0]
 test_features = data_utils.extract_significant_features(test_data)
 
 classifiers = [
-    LogisticRegression(max_iter=1000),
+    LogisticRegression(),
     KNeighborsClassifier(3),
     SVC(gamma=2, C=1),
     GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=0.01, max_iter=10000),
+    MLPClassifier(alpha=0.1, max_iter=5000),
     AdaBoostClassifier(),
     GaussianNB(),
 ]
